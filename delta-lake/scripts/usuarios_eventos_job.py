786450d0d0f86a9734eda3ba0df8e387dc11fcf8
Leonardo Vasconcellos <leozvasconcellos@gmail.com>
2022-11-01 00:55:03 -0300

    Improved examples

--- delta-lake/scripts/usuarios_eventos_job.py
+++ delta-lake/scripts/usuarios_eventos_job.py
@@ -1,11 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 
-job_name = "usuarios-eventos-job"
 delta_table_name = "`teste_db`.`usuario_evento_delta`"
-table_name = "`teste_db`.`usuario_evento`"
+delta_table_path = "s3://aws-emr-assets-428204489288-us-east-1/databases/teste_db/usuario_evento_delta"
 
-spark = SparkSession.builder.appName(job_name) \
+spark = SparkSession.builder.appName("usuarios-eventos-job") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .config("spark.hadoop.hive.metastore.client.factory.class", "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory") \
@@ -27,13 +26,13 @@ spark.readStream.format("json") \
     .dropDuplicates(subset=["id"]) \
     .writeStream.format("delta") \
     .outputMode("append") \
-    .option("checkpointLocation", "s3://aws-emr-assets-428204489288-us-east-1/checkpoints/" + job_name + "/") \
+    .option("checkpointLocation", delta_table_path + "/_checkpoints/") \
     .trigger(once=True) \
     .toTable(delta_table_name) \
     .awaitTermination()
 
-spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS " + table_name + " (id STRING, data_hora_evento TIMESTAMP, codigo_usuario STRING, nome_usuario STRING) \
+spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS `teste_db`.`usuario_evento` (id STRING, data_hora_evento TIMESTAMP, codigo_usuario STRING, nome_usuario STRING) \
 ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' \
 STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat' \
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat' \
-LOCATION 's3://aws-emr-assets-428204489288-us-east-1/databases/teste_db/usuario_evento_delta/_symlink_format_manifest/'")
+LOCATION '" + delta_table_path + "/_symlink_format_manifest/'")
--- delta-lake/scripts/usuarios_merge_job.py
+++ delta-lake/scripts/usuarios_merge_job.py
@@ -3,11 +3,10 @@ from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 from pyspark.sql.functions import col, desc, row_number
 from delta.tables import DeltaTable
 
-job_name = "usuarios-merge-job"
 delta_table_name = "`teste_db`.`usuario_delta`"
-table_name = "`teste_db`.`usuario`"
+delta_table_path = "s3://aws-emr-assets-428204489288-us-east-1/databases/teste_db/usuario_delta"
 
-spark = SparkSession.builder.appName(job_name) \
+spark = SparkSession.builder.appName("usuarios-merge-job") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .config("spark.hadoop.hive.metastore.client.factory.class", "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory") \
@@ -45,13 +44,13 @@ spark.readStream.format("json") \
     .writeStream.format("delta") \
     .foreachBatch(upsert_to_delta) \
     .outputMode("append") \
-    .option("checkpointLocation", "s3://aws-emr-assets-428204489288-us-east-1/checkpoints/" + job_name + "/") \
+    .option("checkpointLocation", delta_table_path + "/_checkpoints/") \
     .trigger(once=True) \
     .start() \
     .awaitTermination()
 
-spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS " + table_name + " (codigo_usuario STRING, nome_usuario STRING) \
+spark.sql("CREATE EXTERNAL TABLE IF NOT EXISTS `teste_db`.`usuario` (codigo_usuario STRING, nome_usuario STRING) \
 ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' \
 STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat' \
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat' \
-LOCATION 's3://aws-emr-assets-428204489288-us-east-1/databases/teste_db/usuario_delta/_symlink_format_manifest/'")
+LOCATION '" + delta_table_path + "/_symlink_format_manifest/'")
